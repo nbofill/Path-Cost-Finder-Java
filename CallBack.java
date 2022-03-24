a412e5846d38b86b12c8984ba46fe859d5fd478d
nbofill <102327863+nbofill@users.noreply.github.com>
2022-03-24 17:36:21 -0400

    Delete CallBack.java

--- CallBack.java
+++
@@ -1,8 +0,0 @@
-package graphs;
-
-/**
- * Represents the processing we apply to a vertex of a graph.
- */
-public interface CallBack<E> {
-	public void processVertex(String vertex, E vertexData);
-}
